53d1e24f77ee7771b1ac0a18ad683af150333f69
Viktoria <vlg.vsap@gmail.com>
2024-04-15 20:50:04 +0300

    changed tests

--- tests/test_burger.py
+++ tests/test_burger.py
@@ -1,6 +1,7 @@
 from praktikum.burger import Burger
 from praktikum.bun import Bun
 from praktikum.ingredient import Ingredient
+import pytest
 
 
 class TestBurger:
@@ -51,12 +52,14 @@ class TestBurger:
     def test_get_receipt(self):
         bun = Bun("Rye bun", 1.50)
         ingredient1 = Ingredient("Lettuce", "Biocotlets", 0.50)
-        ingredient2 = Ingredient("Tomato", "Biocotlets", 0.75)
 
         burger = Burger()
         burger.set_buns(bun)
         burger.add_ingredient(ingredient1)
-        burger.add_ingredient(ingredient2)
 
-        receipt = burger.get_receipt()
-        assert ingredient1.get_name() in receipt and ingredient2.get_type().lower() in receipt
+        expected_receipt = (f'(==== {bun.get_name()} ====)\n'
+                            f'= {str(ingredient1.get_type()).lower()} {ingredient1.get_name()} =\n'
+                            f'(==== {bun.get_name()} ====)\n'
+                            f'\nPrice: {burger.get_price()}')
+
+        assert expected_receipt == burger.get_receipt()
